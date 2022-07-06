19db5e206291f0bd548afc10d2945b00ca76e6d7
ernestlipson <ernestlipson@gmail.com>
2022-07-06 10:05:02 +0000

    beginning files

---
+++ lib/splashscreen.dart
@@ -0,0 +1,13 @@
+class MyScreen extends StatefulWidget {
+  const MyScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MyScreen> createState() => _MyScreenState();
+}
+
+class _MyScreenState extends State<MyScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
